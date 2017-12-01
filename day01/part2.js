c4acf604e138c9ad4a61ce35ab90a3f377a407db
Jeff Raymakers <jephly@gmail.com>
2017-11-30 21:09:27 -0800

    day1

---
+++ day01/part2.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(process.argv[2], 'utf8');
+const lines = data.split('\n');
+const digits = lines[0];
+
+let sum = 0;
+for (let i = 0; i < digits.length; i++) {
+  const digit = digits[i];
+  const nextDigit = digits[(i + (digits.length/2)) % digits.length];
+  if (digit === nextDigit) {
+    const digitNum = parseInt(digit, 10);
+    sum += digitNum;
+  }
+}
+
+console.log(sum);
