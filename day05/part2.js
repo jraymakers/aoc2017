4845b39e023bdf328accc811e084962646b5062b
Jeff Raymakers <jephly@gmail.com>
2017-12-04 21:05:25 -0800

    day 5

---
+++ day05/part2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(process.argv[2], 'utf8');
+const lines = data.split('\n');
+
+const jumps = lines.map((line) => parseInt(line, 10));
+
+let steps = 0;
+let current = 0;
+while (current < jumps.length) {
+  const offset = jumps[current];
+  if (offset >= 3) {
+    jumps[current] -= 1;
+  } else {
+    jumps[current] += 1;
+  }
+  current += offset;
+  steps += 1;
+}
+
+console.log(steps);
