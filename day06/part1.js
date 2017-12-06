5659409dd691f88b355a958c4ca75fe836e184e8
Jeff Raymakers <jephly@gmail.com>
2017-12-05 21:12:49 -0800

    day 6

---
+++ day06/part1.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(process.argv[2], 'utf8');
+const numberStrings = data.split('\t');
+const numbers = numberStrings.map((numStr) => parseInt(numStr, 10));
+
+function maxIndex(nums) {
+  let max = -Infinity;
+  let maxi = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i]
+      maxi = i;
+    }
+  }
+  return maxi;
+}
+
+function hash(nums) {
+  return nums.map((num) => num.toString(16)).join('');
+}
+
+function solve(nums) {
+  let steps = 0;
+  let seen = {};
+
+  while (true) {
+    const h = hash(nums);
+    console.log(h);
+    if (seen[h]) {
+      return steps;
+    }
+    seen[h] = true;
+    let index = maxIndex(nums);
+    let count = nums[index];
+    nums[index] = 0;
+    while (count > 0) {
+      index = (index + 1) % nums.length;
+      nums[index] += 1;
+      count -= 1;
+    }
+    steps += 1;
+  }
+}
+
+console.log(solve(numbers));
